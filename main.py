63adcdea851f1dfb0cbbda09924b8d4e44c2b4a9
damanprxxt <damanpreet.singh73@gmail.com>
2023-05-23 20:09:56 -0400

    "Wikipedia Scientist Scraper Robot"

--- main.py
+++ main.py
@@ -8,9 +8,31 @@ robot = Robot("Quandrinaut")
 def introduce_yourself():
     robot.say_hello()
 
+def print_summary(birth,death,summary,scientistName):
+    print("\n\nScientist Name: ",scientistName)
+    print("Birth: ",birth)
+    print("Death: ",death)
+    print("Age: ",death-birth)
+    print("Summary : ",summary)
+    print("\n\n\n")
+    
+    
+    
+def search(scientistName):
+    # for scientist in SCIENTISTS:
+    robot.search_scientist_by_name(scientistName)
+    birth_year=robot.get_birth_date()
+    death_year=robot.get_death_date()
+    age=death_year-birth_year
+    summary=robot.get_summary()
+    print_summary(birth_year,death_year,summary,scientistName)
+    
 
 def main():
     introduce_yourself()
+    robot.open_webpage("https://www.wikipedia.org")
+    for scientist in SCIENTISTS:
+        search(scientist)
 
 
 if __name__ == "__main__":
--- robotics.py
+++ robotics.py
@@ -1,4 +1,8 @@
+from time import sleep
 from RPA.Browser.Selenium import Selenium
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import re
 
 
 br = Selenium()
@@ -16,3 +20,35 @@ class Robot:
 
     def open_webpage(self, webpage):
         br.open_available_browser(webpage)
+    
+    def search_scientist_by_name(self, scientistName):
+        self.open_webpage("https://www.wikipedia.org")
+        search_input = br.driver.find_element(By.NAME,"search")
+        search_input.clear()
+        search_input.send_keys(scientistName)
+        search_input.send_keys(Keys.RETURN)
+    
+    def get_birth_date(self):
+        dates = br.driver.find_element(By.CLASS_NAME,'infobox').find_elements(By.CLASS_NAME,'infobox-data')  
+        birthdate_text = dates[0].text
+        birthdate_text=birthdate_text.replace('\n',' ')
+        for i in birthdate_text.split(" "):
+            if(len(i)==4 and re.match("\d{4}",i)):
+                return int(i)
+    
+    def get_death_date(self):
+        deathdate_text = br.driver.find_element(By.CLASS_NAME,'infobox').find_elements(By.CLASS_NAME,'infobox-data')[1].text
+        deathdate_text=deathdate_text.replace('\n',' ')
+        for i in deathdate_text.split(" "):
+            if(len(i)==4 and re.match("\d{4}",i)):
+                return int(i)
+    
+    def get_summary(self):
+        i=2
+        while(i<20):
+            s="//p["+str(i)+"]"
+            summary = br.driver.find_element(By.XPATH,s).text
+            if(len(summary.strip())<20):
+                i=i+1
+                continue
+            return summary
